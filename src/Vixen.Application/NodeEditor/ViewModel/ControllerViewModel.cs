5a0a4853fb9be53626951e1d97e75b0cf6c9dd0a
ShadowLight8 <stuff@nickandbeth.com>
2025-01-11 10:00:45 -0500

    Progress towards UI and data bindings

---
+++ src/Vixen.Application/NodeEditor/ViewModel/ControllerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VixenApplication.NodeEditor.Core;
+
+namespace VixenApplication.NodeEditor.ViewModel
+{
+	internal class ControllerViewModel : ObservableObject
+	{
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set 
+			{ 
+				_name = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+}
---
+++ src/Vixen.Application/NodeEditor/ViewModel/NodeEditorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VixenApplication.NodeEditor.Core;
+
+namespace VixenApplication.NodeEditor.ViewModel
+{
+	internal class NodeEditorViewModel : ObservableObject
+	{		
+		public NodeEditorViewModel()
+		{
+		}
+	}
+}
